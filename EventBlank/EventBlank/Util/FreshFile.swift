7ccc771b034012f102b6e5c8b7681afb6390bdf1
Marin Todorov <marin@underplot.com>
2015-07-14 21:28:29 +0200

    * https://github.com/icanzilb/EventBlankApp/issues/20 * first complete version of in-app updates

--- EventBlank/EventBlank/Util/FreshFile.swift
+++ EventBlank/EventBlank/Util/FreshFile.swift
@@ -10,22 +10,21 @@ import Foundation
 
 //TODO: relatively old Swift code, wait up for Swift 2.0 to rearrange the error handling
 
-class FreshFile: Printable {
+//MARK: - FreshInfo
+struct FreshInfo {
+    var etag: String?
+    var contentLength: Double?
+    var tempPath: String?
+}
 
-    //MARK: - FreshInfo
-    struct FreshInfo {
-        var etag: String?
-        var contentLength: Double?
-        var tempPath: String?
-    }
+class FreshFile: Printable {
 
     //MARK: - FreshFile properties
     var remoteURL: NSURL!
     var localURL: NSURL!
     
-    var refreshRate: Double = 5 * 60.0
+    var refreshRate: Double = 5 * 20.0
     var networkRetryInterval = 0.1 * 60.0
-    var replaceRetryInterval = 0.15 * 60.0
     
     var currentModifyDate = NSDate(timeIntervalSince1970: 0.0)
     
@@ -44,13 +43,15 @@ class FreshFile: Printable {
     }
     
     //MARK: - actions
-    var willDownloadFileMap = OrderedMap<String, (FreshInfo)->Bool>()
+    typealias BoolClosure = (Bool)->Void
+    
+    var willDownloadFileMap = OrderedMap<String, (FreshInfo, BoolClosure)->Void>()
     var willReplaceFileMap = OrderedMap<String, (FreshInfo)->Bool>()
-    var didReplaceFileMap = OrderedMap<String, (Bool)->Void>()
+    var didReplaceFileMap = OrderedMap<String, BoolClosure>()
     
     static var actionCounter = 0
     
-    func addAction(#willDownloadFile: (FreshInfo)->Bool, withKey: String?) {
+    func addAction(#willDownloadFile: (FreshInfo, BoolClosure)->Void, withKey: String?) {
         willDownloadFileMap[withKey ?? "default-\(++self.dynamicType.actionCounter)"] = willDownloadFile
     }
     
@@ -58,7 +59,7 @@ class FreshFile: Printable {
         willReplaceFileMap[withKey ?? "default-\(++self.dynamicType.actionCounter)"] = willReplaceFile
     }
     
-    func addAction(#didReplaceFile: (Bool)->Void, withKey: String?) {
+    func addAction(#didReplaceFile: BoolClosure, withKey: String?) {
         didReplaceFileMap[withKey ?? "default-\(++self.dynamicType.actionCounter)"] = didReplaceFile
     }
     
@@ -83,7 +84,12 @@ class FreshFile: Printable {
     private var refreshUUID: String?
     
     func refresh() {
-        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+            self.refreshBackground()
+        })
+    }
+    
+    func refreshBackground() {
         let localRefreshUUID = NSUUID().UUIDString
         refreshUUID = localRefreshUUID
         
@@ -137,8 +143,7 @@ class FreshFile: Printable {
             }
             
             if let response = response as? NSHTTPURLResponse {
-                //println(response.allHeaderFields)
-                
+
                 if let newEtag = response.allHeaderFields["Etag"] as? String {
                     
                     println("compare local \(self.currentEtag) to \(newEtag)")
@@ -151,12 +156,14 @@ class FreshFile: Printable {
                         
                         fileInfo.etag = newEtag
                         
-                        if let contentLength = response.allHeaderFields["Etag"] as? String {
+                        if let contentLength = response.allHeaderFields["Content-Length"] as? String {
                             fileInfo.contentLength = (contentLength as NSString).doubleValue
                             println("about to download \(fileInfo.contentLength) bytes")
                         }
                         
                         result = true
+                    } else {
+                        println("event file is up to date")
                     }
                     
                 } else {
@@ -186,17 +193,33 @@ class FreshFile: Printable {
         
         println("will download file")
         
-        for (key, handler) in willDownloadFileMap {
-            if handler(info) == false {
-                return //a handler cancelled the download
+        var semaphore = dispatch_semaphore_create(0)
+        var shouldContinueWithDownload = true
+        
+        //ask all interested parties if they allow the download
+        for (key, handler) in self.willDownloadFileMap {
+            
+            handler(info, {result in
+                shouldContinueWithDownload = shouldContinueWithDownload && result
+                dispatch_semaphore_signal(semaphore)
+            })
+            
+            //wait for the target to respond about the download
+            while (dispatch_semaphore_wait(semaphore, DISPATCH_TIME_NOW)>0) {
+                NSRunLoop.currentRunLoop().runMode(NSDefaultRunLoopMode, beforeDate: NSDate(timeIntervalSinceNow: 0))
+            }
+            
+            if !shouldContinueWithDownload {
+                println("update download cancelled")
+                return //somebody cancelled the download
             }
         }
         
         let downloadTask = NSURLSession.sharedSession().downloadTaskWithURL(remoteURL, completionHandler: {tempUrl, response, error in
             
-            println(tempUrl)
-            println(response)
-            println(error)
+            //println(tempUrl)
+            //println(response)
+            //println(error)
             
             //copy the downloaded file to a temp location
             var tempCopyError: NSError?
@@ -210,10 +233,15 @@ class FreshFile: Printable {
             }
 
             info.tempPath = tempCopyPath
-            self.replaceFile(info: info)
+            
+            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+                self.replaceFile(info: info)
+            })
         })
         
         downloadTask.resume()
+        
+        println("started update file download")
     }
     
     func delay(#seconds: Double, completion:()->()) {
@@ -251,10 +279,7 @@ class FreshFile: Printable {
         //check if allowed to replace file
         for (key, handler) in self.willReplaceFileMap {
             if handler(info) == false {
-                delay(seconds: replaceRetryInterval, completion: {
-                    self.replaceFile(info: info)
-                })
-                return //retry later
+                return //retry on next ping to remote file
             }
         }
         
@@ -274,7 +299,6 @@ class FreshFile: Printable {
                 handler(replaceSuccess)
             }
         })
-        
     }
     
     var description: String {
--- EventBlank/EventBlank/Util/UpdateManager.swift
+++ EventBlank/EventBlank/Util/UpdateManager.swift
@@ -7,19 +7,29 @@
 //
 
 import Foundation
+import UIKit
+
+let kPendingUpdateChangedNotification = "kPendingUpdateChangedNotification"
 
 class UpdateManager: NSObject {
     
-    var updateCheckInterval = 1.0 * 15.0
+    var updateCheckInterval = 1.0 * 60.0
     
     var fileBinder: FreshFile
     
     init(filePath path: FilePath, remoteURL: NSURL, autostart: Bool = true) {
         
         fileBinder = FreshFile(localURL: NSURL(fileURLWithPath: path.filePath)!, remoteURL: remoteURL)
-        fileBinder.refreshRate = updateCheckInterval
-        
+
         super.init()
+
+        fileBinder.refreshRate = updateCheckInterval
+
+        fileBinder.addAction(willDownloadFile: {info, result in
+            dispatch_async(dispatch_get_main_queue(), {
+                self.askUserAboutDownloadingUpdate(info, result: result)
+            })
+        }, withKey: nil)
 
         if autostart {
             delay(seconds: 1.0, {
@@ -34,5 +44,46 @@ class UpdateManager: NSObject {
     
     func stop() {
         self.fileBinder.unbind()
+    }
+    
+    func askUserAboutDownloadingUpdate(info: FreshInfo, result: (Bool)->Void) {
+        println("update manager: will download \(info.etag) update")
+
+        let defaults = NSUserDefaults.standardUserDefaults()
+        
+        let message = String(format: "Download latest version of the event schedule? This will be a %.2f MB download; if you're on cell data charges may apply.", (info.contentLength!/1_000_000))
+        
+        let alert = UIAlertController(title: "Allow Download",
+            message: message,
+            preferredStyle: UIAlertControllerStyle.ActionSheet)
+        
+        let allowAction = UIAlertAction(title: "Allow Download", style: UIAlertActionStyle.Default, handler: {_ in
+            defaults.setValue(false, forKey: "isTherePendingUpdate")
+            defaults.synchronize()
+
+            self.notification(kPendingUpdateChangedNotification, object: nil)
+            
+            result(true)
+        })
+        
+        let skipAction = UIAlertAction(title: "Skip Update", style: UIAlertActionStyle.Cancel, handler: {_ in
+            defaults.setValue(true, forKey: "isTherePendingUpdate")
+            defaults.synchronize()
+            
+            self.notification(kPendingUpdateChangedNotification, object: nil)
+            
+            result(false)
+        })
+        
+        alert.addAction(allowAction)
+        alert.addAction(skipAction)
+        
+        //present on tab view controller
+        let tabViewController = (UIApplication.sharedApplication().delegate as! AppDelegate).window?.rootViewController as! UITabBarController
+        tabViewController.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    func triggerRefresh() {
+        fileBinder.refresh()
     }
 }
