bb6a8a803180917ef26867a5913984c430da317d
Marin Todorov <marin@underplot.com>
2015-08-23 16:25:02 +0200

    * fixed tweet list bottom offset

--- EventBlank/EventBlank/ViewControllers/Feed/TweetListViewController.swift
+++ EventBlank/EventBlank/ViewControllers/Feed/TweetListViewController.swift
@@ -69,6 +69,14 @@ class TweetListViewController: UIViewController, UITableViewDelegate, UITableVie
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
     }
     
+    func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 100.0
+    }
+    
+    func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+    }
+
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         fatalError()
     }
