95250c501ffd9a1fecf5ec01941675c222099273
Marin Todorov <marin@underplot.com>
2016-04-26 10:02:12 +0200

    * added theme around the UI

--- EventBlank/EventBlank2-iOS/AppDelegate.swift
+++ EventBlank/EventBlank2-iOS/AppDelegate.swift
@@ -8,17 +8,7 @@
 
 import UIKit
 import RealmSwift
-
-@UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    var window: UIWindow?
-    var appController = AppController()
-    
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        return true
-    }
-}
+import MAThemeKit
 
 extension UIApplication {
     static var controller: AppController {
@@ -27,5 +17,37 @@ extension UIApplication {
     
     static var interactor: Interactor {
         return (UIApplication.sharedApplication().delegate as! AppDelegate).appController.interactor
+    }
+}
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    var appController = AppController()
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        setupUI()
+        return true
+    }
+}
+
+extension AppDelegate {
+    func setupUI() {
+        window?.backgroundColor = UIColor.whiteColor()
+        
+        let event = EventData.defaultEvent
+        let primaryColor = event.mainColor
+        //let secondaryColor = event.secondaryColor
+        
+        MAThemeKit.customizeNavigationBarColor(primaryColor, textColor: UIColor.whiteColor(), buttonColor: UIColor.whiteColor())
+        MAThemeKit.customizeButtonColor(primaryColor)
+        MAThemeKit.customizeSwitchOnColor(primaryColor)
+        MAThemeKit.customizeSearchBarColor(primaryColor, buttonTintColor: UIColor.whiteColor())
+        MAThemeKit.customizeActivityIndicatorColor(primaryColor)
+        MAThemeKit.customizeSegmentedControlWithMainColor(UIColor.whiteColor(), secondaryColor: primaryColor)
+        MAThemeKit.customizeSliderColor(primaryColor)
+        MAThemeKit.customizePageControlCurrentPageColor(primaryColor)
+        MAThemeKit.customizeTabBarColor(UIColor.whiteColor().mixWithColor(primaryColor, weight: 0.025), textColor: primaryColor)
     }
 }
